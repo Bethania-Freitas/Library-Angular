fa7c49f08c11ec8e860ec1a7fd2439b3e511af39
Bethânia Alves de Oliveira e Freitas <bethaniafreitas@yahoo.com.br>
2024-02-28 21:09:50 -0300

    Reservas pronto

--- src/app/reserva/reserva.component.ts
+++ src/app/reserva/reserva.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Reservas } from './../models/reservas.model';
+import { ReservaService } from './../services/reserva.service';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ReservaService } from '../services/reserva.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reserva',
   templateUrl: './reserva.component.html',
   styleUrl: './reserva.component.scss',
 })
-export class ReservaComponent {}
+export class ReservaComponent implements OnInit {
+  title = 'Reservas';
+
+  reservas$ = new Observable<Reservas[]>();
+
+  id = '';
+  usuario = '';
+  data_inicio = '';
+  data_fim = '';
+
+  constructor(
+    private reservaService: ReservaService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const idFromRoute = params.get('id')!;
+      this.id = idFromRoute;
+      this.obterReservasCadastradas();
+    });
+  }
+
+  obterReservasCadastradas() {
+    this.reservas$ = this.reservaService.obterReservas(this.id);
+  }
+
+  NovaReserva() {}
+}
--- src/app/services/reserva.service.ts
+++ src/app/services/reserva.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Reservas } from '../models/reservas.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservaService {
+  private url = environment.api;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  obterReservas(idLivro: string): Observable<Reservas[]> {
+    return this.httpClient.get<Reservas[]>(`${this.url}/${idLivro}/reservas`);
+  }
 }
